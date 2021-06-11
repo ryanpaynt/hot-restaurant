9d6cc792da8134367d7494f210a2befe11c0c14d
Ryan <ryanpaynt@uncc.edu>
2021-06-11 14:02:40 -0400

    added starwars view.html for a template for hot restaurants front end

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded( {extended: true}));
 app.use(express.json());
 
-app.get('/', (req, res) =>{
-    res.send('<h1 style="color: darkred">Welcome to Hot Restaurant!</h1>')
-});
+app.get('/', (req, res) =>res.sendFile(path.join(__dirname, 'view.html')));
 
 app.listen(PORT, () => {
     console.log(`Listening at http://localhost:${PORT}`);
